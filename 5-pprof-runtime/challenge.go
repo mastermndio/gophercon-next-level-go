fdc91f0e58fb8224b968afbd063411d69de12c01
Aaron Brooks <abrooks@mastermnd.io>
2020-11-10 15:15:47 -0500

    Added Runtime profiling

---
+++ 5-pprof-runtime/challenge.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pkg/profile"
+)
+
+// Create mapping stucture?
+type finalLetterMap map[rune]int
+
+// Create a application that takes a list of strings and lets us know how many times each letter has occured. This should be complete map rather than one for each string.
+func CountLetterFrequency(text string) finalLetterMap {
+	ourMap := finalLetterMap{}
+	for _, letter := range text {
+		ourMap[letter]++
+	}
+
+	return ourMap
+}
+
+// ConcurrentLetterFrequency Create Concurrent function to use the above function
+func ConcurrentLetterFrequency(texts []string) finalLetterMap {
+	c := make(chan finalLetterMap)
+	for _, item := range texts {
+		go func(s string) {
+			c <- CountLetterFrequency(s)
+		}(item)
+	}
+
+	completeMap := finalLetterMap{}
+	for range texts {
+		for k, v := range <-c { //waiting for channel to deliver map
+			completeMap[k] += v
+		}
+	}
+
+	close(c)
+	return completeMap
+}
+
+// Create function to create big map from each individual map
+func main() {
+	defer profile.Start().Stop()
+	texts := []string{"Hello, my name is Aaron",
+		"Goodbye, I will see you later",
+		"What a beautiful day it is"}
+
+	fmt.Println(ConcurrentLetterFrequency(texts))
+}
